9e045058b7f3b1412fe4099af8de209d3761708a
Merhawi Huwarshek <Merhawir8@gmail.com>
2021-05-07 14:43:11 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <PersonCard lastName="Doe" firstName="Jane" age={45} hairColor="Black"/>
       <PersonCard lastName="Smith" firstName="John" age={88} hairColor="Brown"/>
+      <PersonCard lastName="Fillmore" firstName="Millard" age={50} hairColor="Brown"/>
+      <PersonCard lastName="Smith" firstName="Maria" age={62} hairColor="Brown"/>
+
     </div>
   );
 }
